332622364ddf56be88a8120dc063b1295b46d973
Isu <natantrojak@wp.pl>
2024-04-21 20:12:19 +0200

    Added Faction model

---
+++ FactionApi/Model/Faction.cs
@@ -0,0 +1,9 @@
+﻿namespace StalkerApi;
+
+public class Faction
+{
+    public string Name { get; set; }
+    public string Logo { get; set; }
+    public int Leader { get; set; }
+    public int BaseLocationID { get; set; }
+}
