61edb129ad198f137791a9ac274c9ae1b8b07539
Isu <natantrojak@wp.pl>
2024-05-30 18:17:56 +0200

    Mapped two Stalker endpoints

--- StalkerApi/StalkerApi.cs
+++ StalkerApi/StalkerApi.cs
@@ -1,4 +1,7 @@
-﻿using StalkerApi.Domain.Model;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using StalkerApi.Core.Model;
+using StalkerApi.Domain.Model;
 
 namespace StalkerApi;
 
@@ -7,10 +10,22 @@ public static class StalkerApi
     public static void MapStalkerApi(this IEndpointRouteBuilder app)
     {
         app.MapGet("stalker/{id:int}", GetStalkerAsync);
+        app.MapGet("stalker/byLocation/{locationId:int}", GetNearbyStalkersAsync);
     }
 
-    private static async Task GetStalkerAsync(HttpContext context, [AsParameters] StalkerServices stalkerServices)
+    private static async Task<Results<Ok<Stalker>, NotFound>> GetStalkerAsync(HttpContext context, 
+        [AsParameters] StalkerServices stalkerServices, int id)
     {
-        stalkerServices.Context.Stalkers.FindAsync();
+        var stalker = await stalkerServices.Context.Stalkers.FindAsync(typeof(Stalker), new[] { id });
+        if (stalker == null)
+            return TypedResults.NotFound();
+
+        return TypedResults.Ok(stalker);
+    }
+
+    private static async Task<List<Stalker>> GetNearbyStalkersAsync(HttpContext context, 
+        [AsParameters] StalkerServices stalkerServices, int locationId)
+    {
+        return await stalkerServices.Context.Stalkers.Where(x => x.CurrentLocationID == locationId).ToListAsync();
     }
 }
