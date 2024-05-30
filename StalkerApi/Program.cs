87dd54e1d1bf614df7f2256d523bf1212f9ae101
Isu <natantrojak@wp.pl>
2024-05-30 18:17:56 +0200

    Mapped two Stalker endpoints

--- StalkerApi/Program.cs
+++ StalkerApi/Program.cs
@@ -1,3 +1,4 @@
+using StalkerApi;
 using StalkerApi.Core.Model;
 using StalkerApi.Infrastructure;
 
@@ -12,6 +13,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+app.MapStalkerApi();
 
 app.Run();
 
