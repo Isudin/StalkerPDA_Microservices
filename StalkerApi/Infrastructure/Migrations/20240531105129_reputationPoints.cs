89204347a26e64657118836e8ca45b1b30cb8d21
Isu <natantrojak@wp.pl>
2024-05-31 12:52:16 +0200

    Added reputation points migration

---
+++ StalkerApi/Infrastructure/Migrations/20240531105129_reputationPoints.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StalkerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class reputationPoints : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ReputationPoints",
+                table: "Stalkers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ReputationPoints",
+                table: "Stalkers");
+        }
+    }
+}
