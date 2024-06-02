01bd65087540228a57e3d6b361e7e7ecd3fc782f
Isu <natantrojak@wp.pl>
2024-06-02 17:28:03 +0200

    Configured tasks DBContext

---
+++ TasksApi/Infrastructure/TasksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TasksApi.Infrastructure
+{
+    public class TasksDbContext : DbContext
+    {
+        internal DbSet<Task> Tasks { get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.Development.json").Build();
+
+            optionsBuilder.UseMongoDB(config.GetConnectionString("StalkerMongoDb")!,
+                config.GetSection("DatabaseNames")["StalkerMongoDb"]!);
+        }
+    }
+}
