5f97b116f5835401be4da51c48909e0674d83249
Isu <natantrojak@wp.pl>
2024-06-02 12:14:14 +0200

    Rolled Stalker record back to class

--- StalkerApi/Domain/Model/Stalker.cs
+++ StalkerApi/Domain/Model/Stalker.cs
@@ -1,7 +1,15 @@
 ﻿namespace StalkerApi.Core.Model;
 
-public record Stalker(int Id, string Name, string Surname, string Nickname, 
-    StalkerStatus Status, int CurrentLocationID, int ReputationPoints);
+public class Stalker
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Surname { get; set; }
+    public string? Nickname { get; set; }
+    public StalkerStatus Status { get; set; }
+    public int CurrentLocationID { get; set; }
+    public int ReputationPoints { get; set; }
+}
 
 public enum StalkerStatus
 {
