681329a8f5ebc9ef14a5aa59198efdfcce10be5c
Isu <natantrojak@wp.pl>
2024-06-02 16:53:05 +0200

    Moved faction class to different folder

---
+++ FactionApi/Domain/Model/Faction.cs
@@ -0,0 +1,10 @@
+﻿namespace FactionApi.Domain.Model;
+
+public class Faction
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Logo { get; set; }
+    public int Leader { get; set; }
+    public int HQLocationID { get; set; }
+}
--- FactionApi/Model/Faction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StalkerApi;
-
-public class Faction
-{
-    public string Name { get; set; }
-    public string Logo { get; set; }
-    public int Leader { get; set; }
-    public int BaseLocationID { get; set; }
-}
